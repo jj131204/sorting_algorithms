8eb99033ca6019363420b9e18da8cf8405b87871
jj131204 <jj131204@gmail.com>
2021-06-14 15:02:27 +0000

    test

--- sort.h
+++ sort.h
@@ -5,11 +5,6 @@
 #include <stdlib.h>
 
 
-/*functions*/
-
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -24,5 +19,19 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+    functions
+*/
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+
+/**
+    prototypes
+*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
