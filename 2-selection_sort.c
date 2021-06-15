9da8cce93b1af4dc6506894381b06a4cf600e78f
jblanco75 <jblanco75@gmail.com>
2021-06-15 00:15:16 +0000

    latest changes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,9 +22,8 @@ void swap(int *x, int *y)
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, minimum;
+	int i, j, minimum;
 
-<<<<<<< HEAD
 	for (i = 0; i < size - 1; i++)
 	{
 		minimum = i;
@@ -34,29 +33,6 @@ void selection_sort(int *array, size_t size)
 
 		swap(&array[minimum], &array[i]);
 		if (array[minimum] != array[i])
-=======
-	if (!array || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		j1 = i;
-		arr = array[i];
-		for (j = i; j < size; j++)
-		{
-			if (arr > array[j])
-			{
-				count = 1;
-				j1 = j;
-				arr = array[j];
-			}
-		}
-		if (count == 1)
-		{
-			swap = array[i];
-			array[i] = arr;
-			array[j1] = swap;
->>>>>>> 6034dad00264864c71042f44f2528d5f529617e5
 			print_array(array, size);
 	}
 }
